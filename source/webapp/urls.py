79cd529c8ed1fcd71f0a8f9475cce4ec145de539
Abylay Kuvatov <abukuvatov@icloud.com>
2023-02-27 16:19:00 +0600

    Добавил страницу детального просмотра товара

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from webapp.views.products import products_view
+from webapp.views.products import products_view, product_details
 
 urlpatterns = [
     path("__debug__/", include('debug_toolbar.urls')),
-    path("", products_view, name="products_view")
+    path("", products_view, name="products_view"),
+    path("product/<int:pk>", product_details, name="product_details")
 ]
--- source/webapp/views/products.py
+++ source/webapp/views/products.py
@@ -1,5 +1,5 @@
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from webapp.models import Product, CategoryChoice
 
@@ -7,7 +7,15 @@ from webapp.models import Product, CategoryChoice
 # Create your views here.
 def products_view(request: WSGIRequest):
     context = {
-        'products': Product.objects.all(),
+        'products': Product.objects.exclude(is_deleted=True).order_by('category', 'name'),
         'categories': CategoryChoice.choices
     }
     return render(request, 'products_view.html', context=context)
+
+
+def product_details(request: WSGIRequest, pk: int):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product_details.html', context={
+        'product': product,
+        'categories': CategoryChoice.choices
+    })
