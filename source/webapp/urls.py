b6043f0c02e3f10ea90c114eaca0d33f01a50f3e
Abylay Kuvatov <abukuvatov@icloud.com>
2023-02-28 14:30:30 +0600

    Добавил js скрипт, который автоматически расширяет высоту textarea, а также внес изменения в стилизацию страниц

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -4,7 +4,8 @@ from webapp.views.products import products_view, product_details, product_add, p
 
 urlpatterns = [
     path("__debug__/", include('debug_toolbar.urls')),
-    path("", products_view, name="products_view"),
+    path("", products_view, name="home"),
+    path("products", products_view, name="products_view"),
     path("product/<int:pk>", product_details, name="product_details"),
     path("product/add", product_add, name="product_add"),
     path("product/<int:pk>/edit", product_edit, name="product_edit"),
